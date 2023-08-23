be1feacc41d9d2ddaaac3680bffd1b129a0f492c
a.shakhovnina <a.shakhovnina@iponweb.net>
2023-08-23 19:05:47 +0300

    move locators to dedicated page

---
+++ ui_tests/query_page.py
@@ -0,0 +1,44 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import logging
+logger = logging.getLogger("test sql executor")
+
+
+class QueryPage:
+    def __init__(self, bro):
+        self.bro = bro
+
+    update_result = By.XPATH, "//div[.='You have made changes to the database. Rows affected: 1']"
+    code_area = (By.CSS_SELECTOR, '.cm-m-sql:nth-child(4)')
+    code_input = (By.CSS_SELECTOR, "textarea:nth-child(1)")
+    run_button = (By.CSS_SELECTOR, ".ws-btn")
+    accept_choices = (By.ID, "accept-choices")
+
+    def send_query(self, query):
+        self.bro.find_element(*self.code_area).click()
+        code_input = self.bro.find_element(*self.code_input)
+        actions = ActionChains(self.bro)
+        actions.move_to_element(code_input).double_click().click_and_hold().send_keys(
+            Keys.CLEAR
+        ).send_keys(query).perform()
+        self.bro.find_element(*self.run_button).click()
+        logging.info(f"Running query: {query}")
+
+    def wait_for_expected_result(self, expected_result):
+        expected_result = By.XPATH, f"//tr/td[.='{expected_result}']"
+        try:
+            WebDriverWait(self.bro, 10).until(EC.presence_of_element_located(expected_result))
+            return 'Found'
+        except:
+            TimeoutError
+
+    def check_update_status(self):
+        try:
+            WebDriverWait(self.bro, 10).until(
+            EC.presence_of_element_located(self.update_result))
+            return 'Found'
+        except:
+            TimeoutError
--- ui_tests/test_sql_runner.py
+++ ui_tests/test_sql_runner.py
@@ -1,21 +1,14 @@
 import pytest
-import logging
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-logger = logging.getLogger("test sql executor")
+from query_page import QueryPage
 
 
 class Browser:
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--headless")
-        self.driver = webdriver.Chrome()
-        logging.info("Running Chrome")
+        self.driver = webdriver.Chrome() # options=chrome_options
 
     def open_test_page(self, url):
         self.driver.get(url)
@@ -27,7 +20,7 @@ def bro():
     bro.open_test_page(
         "https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all"
     )
-    bro.driver.find_element(By.ID, "accept-choices").click()
+    bro.driver.find_element(*QueryPage.accept_choices).click()
     yield bro.driver
     bro.driver.close()
 
@@ -44,35 +37,22 @@ query_2 = """
         """
 
 
-class QueryPage:
-    def __init__(self, bro):
-        self.bro = bro
-
-    def send_query(self, query):
-        self.bro.find_element(By.CSS_SELECTOR, '.cm-m-sql:nth-child(4)').click()
-        code_input = self.bro.find_element(By.CSS_SELECTOR, "textarea:nth-child(1)")
-        actions = ActionChains(self.bro)
-        actions.move_to_element(code_input).double_click().click_and_hold().send_keys(
-            Keys.CLEAR
-        ).send_keys(query).perform()
-        self.bro.find_element(By.CSS_SELECTOR, ".ws-btn").click()
-        logging.info(f"Running query: {query}")
-
-
 @pytest.mark.parametrize(
     "query, expected_result", [(query_1, "Via Ludovico il Moro 22"), (query_2, "6")]
 )
 def test_select_data(bro, query, expected_result):
     QueryPage(bro).send_query(query)
-    expected_result = By.XPATH, f"//tr/td[.='{expected_result}']"
-    assert WebDriverWait(bro, 10).until(
-        EC.presence_of_element_located(expected_result)
-    ), f"Expected result {expected_result} not found "
+    assert QueryPage(bro).wait_for_expected_result(expected_result) == 'Found', \
+        f"Expected result {expected_result} not found "
 
-update_query_1 = "INSERT INTO Customers (CustomerName, City, Country) VALUES ('Test Customer', 'Test city', 'Test country');"
-verification_query_1 = "SELECT count(*) FROM Customers WHERE CustomerName='Test Customer' AND city='Test city' AND country='Test country';"
-update_query_2 = "INSERT INTO Customers (CustomerName, City, Country) VALUES ('Test Customer', 'Test city', 'Test country');"
-verification_query_2 = "SELECT count(*) FROM Customers WHERE CustomerName='Test Customer' AND city='Test city' AND country='Test country'"
+update_query_1 = "INSERT INTO Customers (CustomerName, City, Country) VALUES ('Test Customer', " \
+                 "'Test city', 'Test country');"
+verification_query_1 = "SELECT count(*) FROM Customers WHERE CustomerName='Test Customer' AND " \
+                       "city='Test city' AND country='Test country';"
+update_query_2 = "INSERT INTO Customers (CustomerName, City, Country) VALUES ('Test Customer', " \
+                 "'Test city', 'Test country');"
+verification_query_2 = "SELECT count(*) FROM Customers WHERE CustomerName='Test Customer' AND " \
+                       "city='Test city' AND country='Test country'"
 
 
 
@@ -82,15 +62,11 @@ verification_query_2 = "SELECT count(*) FROM Customers WHERE CustomerName='Test 
 )
 def test_update_data(bro, update_query, verification_query, expected_result):
     QueryPage(bro).send_query(update_query)
-    updated_note = By.XPATH, "//div[.='You have made changes to the database. Rows affected: 1']"
-    assert WebDriverWait(bro, 10).until(
-        EC.presence_of_element_located(updated_note)
-    ), f"Expected result {updated_note} not found "
+    assert QueryPage(bro).check_update_status() == 'Found', \
+        f"The data was not updated correctly"
     QueryPage(bro).send_query(verification_query)
-    expected_result = By.XPATH, f"//tr/td[.='{expected_result}']"
-    assert WebDriverWait(bro, 10).until(
-        EC.presence_of_element_located(expected_result)
-    ), f"Expected result {expected_result} not found "
+    assert QueryPage(bro).wait_for_expected_result(expected_result) == 'Found', \
+        f"Expected result {expected_result} not found "
 
 
 
